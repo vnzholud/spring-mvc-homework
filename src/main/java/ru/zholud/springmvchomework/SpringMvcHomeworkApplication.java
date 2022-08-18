0646c10153d2f530a96fad7f6c906c35c342bf83
ZholudV <goldexzholud@gmail.com>
2022-08-18 18:41:30 +0500

    первый комит

---
+++ src/main/java/ru/zholud/springmvchomework/SpringMvcHomeworkApplication.java
@@ -0,0 +1,13 @@
+package ru.zholud.springmvchomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/zholud/springmvchomework/SpringMvcHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zholud.springmvchomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
